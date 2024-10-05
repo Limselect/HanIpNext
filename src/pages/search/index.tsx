39b6004741c2ddf60c8f0a2de679f165d6128202
Seontaek <seontaek1998@gmail.com>
2024-10-05 18:48:30 +0900

    cinema project 2nd step

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,15 @@
+import SearchableLayout from "@/pages/components/searchable-layout";
 import { useRouter } from "next/router";
+import { ReactNode } from "react";
 
 export default function Page() {
   const router = useRouter();
 
   const { q } = router.query;
 
-  return <h1>검색결과 : {q}</h1>;
+  return <h2>{q}를 검색 했습니다</h2>;
 }
+
+Page.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
